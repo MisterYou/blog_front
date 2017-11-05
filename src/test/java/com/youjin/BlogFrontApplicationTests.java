5cb63fef9c58450430c33444ccb50e9f13c7504a
ZhongWanYuXi <youjiniot@163.com>
2017-11-05 20:51:22 +0800

    11

---
+++ src/test/java/com/youjin/BlogFrontApplicationTests.java
@@ -0,0 +1,16 @@
+package com.youjin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogFrontApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
